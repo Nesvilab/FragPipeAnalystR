f641e6f7e9fbfa101dbefa9603ccd4294837f9ff
hsiao yi <yihsiao@umich.edu>
2025-07-27 11:52:25 -0400

    fix average_samples of not using the correct variable

--- R/data_manipulation.R
+++ R/data_manipulation.R
@@ -6,7 +6,7 @@ remove_samples <- function(se, samples){
 
 #' @export
 average_samples <- function(se, samples, keep_label){
-  assay(se)[, keep_label] <- rowMeans(assay(temp), na.rm = T)
+  assay(se)[, keep_label] <- rowMeans(assay(se)[,samples], na.rm = T)
   rm_samples <- samples[!samples %in% keep_label]
   se <- remove_samples(se, rm_samples)
   return(se)
